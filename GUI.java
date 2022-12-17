5f09dadf2322210db5ec70e3dfcfa38116c8bc6a
Marie Cari <mcari@nyc.yearup.org>
2022-12-17 14:58:08 -0500

    basic frame and panel created

--- GUI.java
+++ GUI.java
@@ -1,2 +1,19 @@
+import javax.swing.*;
+
 public class GUI {
+
+    public static void main(String[] args) {
+        JPanel panel = new JPanel();
+
+        JFrame frame = new JFrame();
+        frame.setSize(100, 100);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.add(panel);
+
+        panel.setLayout(null);
+
+    }
+
+
 }
