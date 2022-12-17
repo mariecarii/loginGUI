96fcee0451fae7d89548ace852c439fcfae0abae
Marie Cari <mcari@nyc.yearup.org>
2022-12-17 15:04:11 -0500

    created username label and text box

--- GUI.java
+++ GUI.java
@@ -6,12 +6,25 @@ public class GUI {
         JPanel panel = new JPanel();
 
         JFrame frame = new JFrame();
-        frame.setSize(100, 100);
+        frame.setSize(400, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         frame.add(panel);
 
         panel.setLayout(null);
+
+        JLabel userLabel = new JLabel("User");
+        userLabel.setBounds(10, 20, 80, 25);
+        panel.add(userLabel);
+
+        JTextField userText = new JTextField();
+        userText.setBounds(100, 20, 165, 25);
+        panel.add(userText);
+
+
+
+
+
 
     }
 
